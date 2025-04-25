cad782adb9bc6de7b6e4b17f786a2cb23e8ff435
Jennifer Nguyen <jennifernguyen@Jennifers-MacBook-Air.local>
2025-04-25 10:23:26 -0700

    Build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/test/",
+  build: {
+    outDir: 'docs'
+  },
   css: {
     postcss: {
       plugins: [tailwind()],
