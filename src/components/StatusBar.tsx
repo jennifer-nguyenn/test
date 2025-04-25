00f7faa8a000808d297cabafe4bbaf4c24786275
Jennifer Nguyen <jennifernguyen@Jennifers-MacBook-Air.local>
2025-04-25 12:58:46 -0700

    Refine status bar styling to match iOS exactly

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const StatusBar: React.FC = () => {
-  const time = "9:41"; // You can make this dynamic if needed
-
   return (
     <div className="status-bar">
       <div className="status-bar-left">
-        <span className="status-bar-time">{time}</span>
+        <span className="status-bar-time">9:41</span>
       </div>
       
       <div className="status-bar-right">
@@ -16,13 +14,11 @@ const StatusBar: React.FC = () => {
           <div className="signal-bar"></div>
           <div className="signal-bar"></div>
         </div>
-        
         <div className="status-bar-wifi">
           <div className="wifi-arc"></div>
           <div className="wifi-arc"></div>
           <div className="wifi-arc"></div>
         </div>
-        
         <div className="status-bar-battery">
           <div className="battery-level"></div>
           <div className="battery-cap"></div>
